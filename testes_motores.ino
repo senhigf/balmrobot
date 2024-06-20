639c1c962ef3bccdbf900e9d4ad9dfcfac0a5d11
senhigf <166862791+senhigf@users.noreply.github.com>
2024-06-20 16:48:34 -0300

    Update testes_motores.ino

--- testes_motores.ino
+++ testes_motores.ino
@@ -1,31 +1,40 @@
-void teste_movimentos(){
-  foward();
-  delay(1000);
-  backwards();
-  delay(1000);
-  RightTurning();
-  delay(1000);
-  LeftTurning();
-  delay(1000);
-}
+ /*void teste_movimentos(){
+   foward();
+   delay(1000);
+   backwards();
+   delay(1000);
+   LeftDiagonal();
+   delay(1000);
+   RightDiagonal();
+   delay(1000);
+ }*/
 
-void CrabWalk(){
-  foward();
-  delay(1000);
-  LeftCrabWalk();
-  delay(1000);
-  backwards();
-  delay(1000);
-  RightCrabWalk();
-}
+// void CrabWalk(){
+//   foward();
+//   delay(1000);
+//   LeftCrabWalk();
+//   delay(1000);
+//   backwards();
+//   delay(1000);
+//   RightCrabWalk();
+// }
 
-void mostrarSensores(){
-  Serial.print(sleft);
-  Serial.print(" - ");
-  Serial.print(scenterleft);
-  Serial.print(" - ");
-  Serial.print(scenterright);
-  Serial.print(" - ");
-  Serial.println(sright);
-}
-
+ void mostrarSensores(){
+   Serial.print(sleft);
+   Serial.print(" - ");
+   Serial.print(scenterleft);
+   Serial.print(" - ");
+   Serial.print(scenterright);
+   Serial.print(" - ");
+   Serial.println(sright);
+ }
+   void lerSensores(){
+   sleft = analogRead(sensorE);
+   delay(10);
+   scenterleft = analogRead(sensorCE);
+   delay(10);
+   scenterright  = analogRead(sensorCD);
+   delay(10);
+   sright = analogRead(sensorD);
+   delay(10);
+ }
